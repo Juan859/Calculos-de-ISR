b78b7e2171e4bc187114ec96960c243599c1a894
Juan C. Mateo Peña <juancarlosmateopena@outlook.com>
2023-09-01 09:10:58 -0400

    Agregar archivos de proyecto.

---
+++ Calculos de ISR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos_de_ISR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
